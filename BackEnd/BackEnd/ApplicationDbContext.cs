f38322fbf09d51bdb0d26fdc6c573dd4bb45e950
Nicolás Snider <nicolas.snider@soulit.io>
2020-10-20 20:34:59 -0300

    Hasta video 9

---
+++ BackEnd/BackEnd/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options): base(options)
+        {
+
+        }
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+    }
+}
