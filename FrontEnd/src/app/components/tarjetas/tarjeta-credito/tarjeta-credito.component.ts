9e608b0fcedd4bce02c355c2f772d23ac71c4c91
Nicolás Snider <nicolas.snider@soulit.io>
2020-10-21 20:50:55 -0300

    hasta 18

--- FrontEnd/src/app/components/tarjetas/tarjeta-credito/tarjeta-credito.component.ts
+++ FrontEnd/src/app/components/tarjetas/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { TarjetasComponent } from '../tarjetas.component';
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TarjetaCreditoComponent implements OnInit {
 
-  constructor() { }
+  form:FormGroup;
+
+  constructor(private formBuilder:FormBuilder) {
+    this.form = this.formBuilder.group({
+      id:0,
+      titular: ['',[Validators.required]],
+      numeroTarjeta: ['',[Validators.required,Validators.maxLength(16),Validators.minLength(16)]],
+      fechaExpiracion:['',[Validators.required,Validators.maxLength(5),Validators.minLength(5)]],
+      cvv:['',[Validators.required,Validators.maxLength(3),Validators.minLength(3)]],
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  guardarTarjeta(){
+    console.log(this.form);
   }
 
 }
