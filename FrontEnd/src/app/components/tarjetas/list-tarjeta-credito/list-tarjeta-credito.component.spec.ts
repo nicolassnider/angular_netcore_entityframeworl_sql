d395600c359fbfef236e2c63121512aef21f0172
Nicolás Snider <nicolas.snider@soulit.io>
2020-10-20 22:38:45 -0300

    Hasta video 16

---
+++ FrontEnd/src/app/components/tarjetas/list-tarjeta-credito/list-tarjeta-credito.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListTarjetaCreditoComponent } from './list-tarjeta-credito.component';
+
+describe('ListTarjetaCreditoComponent', () => {
+  let component: ListTarjetaCreditoComponent;
+  let fixture: ComponentFixture<ListTarjetaCreditoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ListTarjetaCreditoComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListTarjetaCreditoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
